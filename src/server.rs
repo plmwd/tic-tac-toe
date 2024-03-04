99a04cb0b55592af3d7c02b5b61c725736559cd2
Paul Wood <pmwood@pm.me>
2024-03-03 23:02:54 -0800

    wip

--- src/server.rs
+++ src/server.rs
@@ -9,15 +9,7 @@ use tokio::{
     sync::{broadcast, mpsc, oneshot, Mutex},
 };
 
-use crate::game::{Conclusion, Game, Player};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-enum ClientMessage {
-    Error(String),
-    Chat(String),
-    Game { game: Game, turn: Player },
-    MarkTile(u8),
-}
+use crate::game::{self, Conclusion, Game};
 
 #[derive(Debug)]
 struct Connection {
@@ -33,7 +25,7 @@ impl Connection {
         }
     }
 
-    async fn read_message(&mut self) -> anyhow::Result<Option<ClientMessage>> {
+    async fn read_message(&mut self) -> anyhow::Result<Option<Message>> {
         self.buffer.clear();
         loop {
             match ron::de::from_bytes(&self.buffer) {
@@ -42,8 +34,8 @@ impl Connection {
                     ron::Error::ExpectedDifferentLength { .. } => {}
                     ron::Error::Eof => {}
                     e => {
-                        println!("error reading message {:?}", e);
-                        self.write_message(&ClientMessage::Error(format!("{}", e)))
+                        println!("error reading message {:#?}", e);
+                        self.write_message(&Message::InvalidMessage(format!("{}", e)))
                             .await
                             .unwrap();
                         self.buffer.clear();
@@ -61,7 +53,7 @@ impl Connection {
         }
     }
 
-    async fn write_message(&mut self, mes: &ClientMessage) -> tokio::io::Result<()> {
+    async fn write_message(&mut self, mes: &Message) -> tokio::io::Result<()> {
         self.stream
             .write_all(format!("{}\n", ron::ser::to_string(&mes).unwrap()).as_bytes())
             .await?;
@@ -70,36 +62,60 @@ impl Connection {
     }
 }
 
-async fn handle_connection(mut con: Connection, mut server: ServerHandle) -> anyhow::Result<()> {
+async fn handle_connection(mut con: Connection, server: ServerHandle) -> anyhow::Result<()> {
+    let mut cx = ConnectionContext::Observer;
+
     loop {
-        if let Some(mes) = con.read_message().await.unwrap() {
-            println!("got mes: {:?}", mes);
-            con.write_message(&mes).await.unwrap();
-        } else {
-            println!("con EOF");
-            break;
+        match con.read_message().await.unwrap() {
+            Some(Message::Disconnect) => {
+                println!("disconnect");
+                break;
+            }
+            None => {
+                println!("con EOF");
+                break;
+            }
+            Some(mes) => {
+                println!("got mes: {:?}", mes);
+                let (tx, rx) = oneshot::channel();
+                server.req_tx.send((cx, mes, tx)).await?;
+                let (new_cx, rsp) = rx.await?;
+                cx = new_cx;
+                con.write_message(&rsp).await?;
+            }
         };
     }
 
+    let (tx, _) = oneshot::channel();
+    server.req_tx.send((cx, Message::Disconnect, tx)).await?;
     Ok(())
 }
 
-#[derive(Debug)]
-enum Command {
-    Join,
-    GetInfo,
-    Chat(Player, String),
-    PlayTurn(Player, u8),
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum ConnectionContext {
+    Observer,
+    Player(game::Player),
 }
 
-enum CommandResponse {
-    Joined(Player),
+#[derive(Debug, Clone, Serialize, Deserialize)]
+enum Message {
+    Join,
+    Joined(Option<game::Player>),
+    Disconnect,
+    GetInfo,
     Info(Game),
+    Chat(String),
+    PlayTurn(u8),
     InvalidTile,
     NotYourTurn,
+    InvalidMessage(String),
 }
 
-type Request = (Command, oneshot::Sender<CommandResponse>);
+type Request = (
+    ConnectionContext,
+    Message,
+    oneshot::Sender<(ConnectionContext, Message)>,
+);
 
 // Game flow:
 //  - wait for two connections
@@ -108,7 +124,7 @@ type Request = (Command, oneshot::Sender<CommandResponse>);
 //      - if any connection is lost, the other player automatically wins
 // - additional connections will watch the match
 struct Server {
-    broadcast: broadcast::Sender<ClientMessage>,
+    broadcast: broadcast::Sender<Message>,
     req_rx: mpsc::Receiver<Request>,
     req_tx: mpsc::Sender<Request>,
     num_connections: u32,
@@ -116,7 +132,7 @@ struct Server {
 
 struct ServerHandle {
     req_tx: mpsc::Sender<Request>,
-    broadcast: broadcast::Receiver<ClientMessage>,
+    broadcast: broadcast::Receiver<Message>,
 }
 
 impl Server {
@@ -138,9 +154,31 @@ impl Server {
         }
     }
 
+    fn handle_request(&mut self, (cx, msg, rsp): Request) {
+        use game::Player::{O, X};
+        use ConnectionContext::{Observer, Player};
+
+        let r: (ConnectionContext, Message) = match msg {
+            Message::Join if Observer == cx => {
+                let cm = match self.num_connections {
+                    0 => (Player(X), Message::Joined(Some(X))),
+                    1 => (Player(O), Message::Joined(Some(O))),
+                    _ => (Observer, Message::Joined(None)),
+                };
+                self.num_connections += 1;
+                cm
+            }
+            msg => (cx, msg),
+        };
+
+        _ = rsp.send(r);
+    }
+
     pub async fn run(mut self) -> anyhow::Result<()> {
         let listener = TcpListener::bind("127.0.0.1:6969").await.unwrap();
+        println!("listening on {}...", listener.local_addr()?);
 
+        #[derive(Debug)]
         enum Action {
             NewConnection(TcpStream, SocketAddr),
             Request(Option<Request>),
@@ -160,17 +198,15 @@ impl Server {
                     let handle = self.handle();
                     tokio::spawn(async move { handle_connection(con, handle).await });
                 }
-                Action::Request(Some((cmd, rsp_callback))) => {
-                    println!("got req {:?}", cmd);
-                }
+                Action::Request(Some(req)) => self.handle_request(req),
                 Action::Request(None) => {
                     println!("lost connection");
                 }
-            }
+            };
         }
     }
 }
 
 pub async fn run() {
-    Server::new().run().await;
+    _ = Server::new().run().await;
 }
